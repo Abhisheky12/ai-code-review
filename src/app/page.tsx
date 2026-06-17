d5316ec2268d10abd06a1d4b10b051405cd36f74
Abhisheky12 <a.yadav7088@gmail.com>
2026-06-18 05:17:56 +0530

    chng

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-// import { Button } from "@/components/ui/button";
-// import Logout from "@/module/auth/components/logout";
-// import { requireAuth } from "@/module/auth/utils/auth-utils";
-// import { redirect } from "next/navigation";
-
-// export default async function Home() {
-//   await requireAuth();
-//   return (
-//      redirect('/dashboard')
-//   );
-// }
-
-
+import { Button } from "@/components/ui/button";
+import Logout from "@/module/auth/components/logout";
+import { requireAuth } from "@/module/auth/utils/auth-utils";
 import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect("/dashboard");
+export default async function Home() {
+  await requireAuth();
+  return (
+     redirect('/dashboard')
+  );
 }
+
+
+// import { redirect } from "next/navigation";
+
+// export default function Home() {
+//   redirect("/dashboard");
+// }
