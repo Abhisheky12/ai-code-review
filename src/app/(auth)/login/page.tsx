243a079ad9cac361a3a53485fb350e528be33173
Abhisheky12 <a.yadav7088@gmail.com>
2026-06-18 04:22:04 +0530

    fix: useRef in query-provider

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+// import React from 'react';
+// export const dynamic = "force-dynamic";
+// import LoginUI from '@/module/auth/components/login-ui'; 
+// import { requireUnAuth } from '@/module/auth/utils/auth-utils';
+
+// const Page = async() => {
+//   await requireUnAuth();
+//   return (
+//     <>
+//       <LoginUI/>
+//     </>
+//   );
+// };
+
+// export default Page;
+
+import LoginUI from "@/module/auth/components/login-ui";
+
 export const dynamic = "force-dynamic";
-import LoginUI from '@/module/auth/components/login-ui'; 
-import { requireUnAuth } from '@/module/auth/utils/auth-utils';
 
-const Page = async() => {
-  await requireUnAuth();
-  return (
-    <>
-      <LoginUI/>
-    </>
-  );
-};
-
-export default Page;
+export default function Page() {
+  return <LoginUI />;
+}
