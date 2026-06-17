af9ace4dfcd68be6527b9e67f45da10ad93d8701
Abhisheky12 <a.yadav7088@gmail.com>
2026-06-18 04:47:04 +0530

    fix: useRef in query-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import { Button } from "@/components/ui/button";
-import Logout from "@/module/auth/components/logout";
-import { requireAuth } from "@/module/auth/utils/auth-utils";
+// import { Button } from "@/components/ui/button";
+// import Logout from "@/module/auth/components/logout";
+// import { requireAuth } from "@/module/auth/utils/auth-utils";
+// import { redirect } from "next/navigation";
+
+// export default async function Home() {
+//   await requireAuth();
+//   return (
+//      redirect('/dashboard')
+//   );
+// }
+
+
 import { redirect } from "next/navigation";
 
-export default async function Home() {
-  await requireAuth();
-  return (
-     redirect('/dashboard')
-  );
+export default function Home() {
+  redirect("/dashboard");
 }
-
